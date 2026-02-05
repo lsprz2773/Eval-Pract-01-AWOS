088c2772814e422d6b15c947425aaa2dd79bae2c
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-05 10:43:29 -0600

    schema.sql ready

---
+++ db/schema.sql
@@ -0,0 +1,60 @@
+--LIMPIEZA DE TABLAS Y TIPOS
+DROP TABLE IF EXISTS attendance, grades, enrollments, groups, courses, teachers, students CASCADE;
+DROP TYPE IF EXISTS program_name, attendance_status CASCADE;
+
+--Creacion de tipos
+
+CREATE TYPE program_name AS ENUM ('Software','Science','Physics');
+CREATE TYPE attendance_status AS ENUM ('Present','Absent','Late','Excused');
+
+--Creacion de tablas principales
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    program program_name NOT NULL,
+    enrollment_year DATE NOT NULL
+);
+
+CREATE TABLE teachers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE courses(
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(10) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    credits SMALLINT NOT NULL
+);
+
+--Creacion de tablas de relacion
+CREATE TABLE groups(
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES courses(id) ON DELETE CASCADE,
+    teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+    term VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE enrollments(
+    id SERIAL PRIMARY KEY,
+    student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+    group_id INTEGER REFERENCES groups(id) ON DELETE CASCADE,
+    enrolled_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE grades(
+    id SERIAL PRIMARY KEY,
+    enrollment_id INTEGER REFERENCES enrollments(id) ON DELETE CASCADE,
+    partial1 DECIMAL,
+    partial2 DECIMAL,
+    final DECIMAL
+);
+
+CREATE TABLE attendance(
+    id SERIAL PRIMARY KEY,
+    enrollment_id INTEGER REFERENCES enrollments(id) ON DELETE CASCADE,
+    date DATE NOT NULL,
+    status attendance_status NOT NULL
+);
