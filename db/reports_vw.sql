ea443a844184dd2b5e4a66cf48e0af1f956a6d11
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-05 11:41:21 -0600

    view 1 ready

--- db/reports_vw.sql
+++ db/reports_vw.sql
@@ -1,5 +1,21 @@
 -- VIEW 1 - vw_courses_performance
-SELECT c.
+CREATE OR REPLACE VIEW vw_courses_performance AS
+SELECT 
+    c.name AS curso, 
+    g.term AS grupo, 
+    ROUND(AVG(gr.partial1 + gr.partial2 + gr.final)/3 , 2) AS promedio,
+    COUNT(
+        CASE
+            WHEN ((gr.partial1 + gr.partial2 + gr.final)/3) < 70
+            THEN '1'
+        END
+    ) AS alumnos_reprobados
+FROM courses c
+INNER JOIN groups g ON g.course_id = c.id
+INNER JOIN enrollments e ON e.group_id = g.id
+INNER JOIN grades gr ON gr.enrollment_id = e.id
+GROUP BY c.name, g.term
+ORDER BY curso;
 
 
 -- VIEW 2 - vw_teacher_load
