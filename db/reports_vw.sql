05a87acb8ea87057acccf268b0df4eff9ce0fa82
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-06 00:00:15 -0600

    view 2 ready, view 3 in progress

--- db/reports_vw.sql
+++ db/reports_vw.sql
@@ -19,11 +19,69 @@ ORDER BY curso;
 
 
 -- VIEW 2 - vw_teacher_load
-
+CREATE OR REPLACE VIEW vw_teacher_load AS
+SELECT 
+    t.id AS id_docente,
+    t.name AS docente,
+    g.term AS grado,
+    COUNT(DISTINCT g.id) AS grupos_actuales,
+    COUNT(DISTINCT e.student_id) AS alumnos_totales,
+    COALESCE(ROUND(AVG((gr.partial1 + gr.partial2 + gr.final)/3), 2),0) AS promedio_general
+FROM teachers t
+LEFT JOIN groups g ON g.teacher_id = t.id
+LEFT JOIN enrollments e ON e.group_id = g.id
+LEFT JOIN grades gr ON gr.enrollment_id = e.id
+GROUP BY t.id, t.name, g.term
+HAVING COUNT(DISTINCT g.id) > 0
+ORDER BY grupos_actuales DESC;
 
 
 -- VIEW 3 - vw_students_at_risk
+--CREATE OR REPLACE VIEW vw_students_at_risk AS
+WITH students_metrics AS (
+    SELECT
+        s.id AS id_estudiante,
+        s.name AS estudiante,
+        s.email AS correo,
+        s.program AS programa,
+        c.name AS curso,
+        COALESCE((gr.partial1 + gr.partial2 + gr.final)/3, 0) AS promedio_actual,
+        COUNT(a.id) AS total_asistencias,
+        COUNT(
+            CASE
+                WHEN a.status = 'Present' THEN 1
+            END
+        ) AS conteo_asistencias
+    FROM students s
+    INNER JOIN enrollments e ON s.id = e.student_id
+    INNER JOIN groups g ON g.id = e.group_id
+    INNER JOIN courses c ON c.id = g.course_id
+    LEFT JOIN grades gr ON gr.enrollment_id = e.id
+    LEFT JOIN attendance a on a.enrollment_id = e.id
+    GROUP BY s.id, s.name, s.email, s.program, c.name, g.partial1, g.partial2, g.final
+), risk_calc AS (
+    SELECT
+        *,
+        CASE
+            WHEN total_asistencias > 0 THEN ROUND((conteo_asistencias::numeric/total_asistencias) * 100, 2)
+            ELSE 100
+        END AS porcentaje_asistencia
+    FROM students_metrics
+)
 
+SELECT
+    id_estudiante,
+    estudiante,
+    correo,
+    programa,
+    curso,
+    ROUND(promedio_actual, 2)AS promedio,
+    porcentaje_asistencia,
+    CASE
+        WHEN current_avg < 70 AND attendance_rate < 80 THEN 'Critical: Grade & Attendance'
+        WHEN current_avg < 70 THEN 'Low Grades'
+        WHEN attendance_rate < 80 THEN 'Low Attendance'
+    END AS risk_reason
 
 
 -- VIEW 4 - vw_attendance_by_group
